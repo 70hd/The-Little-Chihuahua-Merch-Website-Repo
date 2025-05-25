0ae8f2aac74673a2054e4fc289f6aebec4128d82
70hd <kanehfernandez@gmail.com>
2025-05-24 18:15:07 -0700

    live webhook testing

--- components/payment-success.tsx
+++ components/payment-success.tsx
@@ -1,24 +1,33 @@
+"use client"; // required to use browser APIs like window
+
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
-
-
 export default function PaymentSuccess() {
-    return (
-      <main className="dynamic-x-padding pt-[12px] pb-42 w-full flex flex-col gap-6 my-24">
+  const [paymentIntentId, setPaymentIntentId] = useState("");
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const id = searchParams.get("payment_intent");
+    if (id) setPaymentIntentId(id);
+  }, []);
+
+  return (
+    <main className="dynamic-x-padding pt-[12px] pb-42 w-full flex flex-col gap-6 my-24">
       <header>
         <h1 className="text-[#16767E] mb-[1.5rem]" tabIndex={0}>
           Thank you for your purchase
         </h1>
         <h2 className="text-[#16767E]" tabIndex={0}>
-          Your order is #00000
+          {paymentIntentId ? `Your order is #${paymentIntentId}` : "Loading order ID..."}
         </h2>
       </header>
       <section className="flex flex-col gap-[16px]">
-        <p>We'll email you an order confirmation wtih details and shipping info</p>
+        <p>We'll email you an order confirmation with details and shipping info</p>
         <Link href="/" className="flex items-center gap-1">
-        <p className="underline">Continue Shopping</p>
+          <p className="underline">Continue Shopping</p>
         </Link>
       </section>
     </main>
-    );
-  }
+  );
+}
